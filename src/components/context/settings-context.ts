992155b56cda477670017f4931a224eba866b312
Lucie Struplova <lstruplova@gmail.com>
2024-05-27 11:36:51 +0200

    Pridani kontextu a checkboxu

--- src/components/context/settings-context.ts
+++ src/components/context/settings-context.ts
@@ -1,12 +1,13 @@
 import { createContext } from "react";
 
-interface PrefData {
+export interface PrefData {
     vegan: boolean;
 }
 
 interface PrefDataSettings extends PrefData {
     setVegan : (vegan:boolean) => void;
 }
+
 export const PrefsContext  = createContext<PrefDataSettings>
     ({
         vegan: true,
